58a58cc16ce2a36fb3415fb5e6645cdeabdd275f
Jay Kakadiya <jaykakadiya2014@gmail.com>
2023-11-11 09:48:38 +0000

    at()

--- js/string.js
+++ js/string.js
@@ -1,1 +1,10 @@
+const sentence = 'The quick brown fox jumps over the lazy dog.';
 
+let index = 5;
+
+console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+// Expected output: "An index of 5 returns the character u"
+
+index = -4;
+
+console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
